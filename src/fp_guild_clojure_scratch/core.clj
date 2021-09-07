1b178ed0b5fb83504697dddbd7297609b0429ddd
Eduardo Santamaria <eduardo@onna.com>
2021-09-07 14:05:51 +0200

    Added idiomatic improvements example as suggested by Dani

--- src/fp_guild_clojure_scratch/core.clj
+++ src/fp_guild_clojure_scratch/core.clj
@@ -1,6 +1,7 @@
-(ns fp-guild-clojure-scratch.core (:gen-class))
+(ns fp-guild-clojure-scratch.core
+  (:gen-class))
 
-(def raw-input (slurp "src/fp_guild_clojure_scratch/input.txt"))
+(def raw-input (slurp (clojure.java.io/resource "input.txt")))
 
 (defn map-vals [f m]
   (into {} (for [[k v] m] [k (f v)])))
@@ -9,7 +10,7 @@
 (defn GCContent? [sequenceNT]
   (let [
         groupedNT (group-by identity sequenceNT)
-        NTcount (into {} (for [[k,v] groupedNT] [k (count v)]))
+        NTcount (into {} (for [[k, v] groupedNT] [k (count v)]))
         GC (+ (NTcount \G 0) (NTcount \C 0))
         total (apply + (vals NTcount))
         ]
@@ -21,7 +22,7 @@
   (->> in
        ((fn [x] (clojure.string/split x #">")))
        (remove clojure.string/blank?)
-       (map (fn [x](clojure.string/split x #"\n" 2)))
+       (map (fn [x] (clojure.string/split x #"\n" 2)))
        (into {})
        (map-vals (fn [x] (clojure.string/replace x "\n" "")))
        )
@@ -29,14 +30,38 @@
 
 
 (defn sequences-GC [in]
-    (map-vals GCContent? (parse-sequences in)
-    )
+  (map-vals GCContent? (parse-sequences in)
+            )
   )
 
 (defn print-result []
-  (println "Result is "(apply max-key val (sequences-GC raw-input)))
+  (println "Result is " (apply max-key val (sequences-GC raw-input)))
   )
 
 (defn -main [& args]
   (print-result)
+  )
+
+(comment
+  "As improved by Dani"
+  (defn gc-content [dna]
+    (let [freqs (frequencies dna)
+          total (count dna)]
+      (float
+        (* 100
+           (/
+             (+ (get freqs \G 0)
+                (get freqs \C 0))
+             total)))))
+
+
+  (defn problem-1 [file]
+    (let [txt-input (slurp (clojure.java.io/resource file))
+          lines (clojure.string/split txt-input #">")
+          cases (map clojure.string/split-lines (rest lines))
+          solutions (for [case cases]
+                      (let [id (first case)
+                            dna (apply str (rest case))
+                            gc (gc-content dna)] {id gc}))]
+      (apply max-key val (into {} solutions))))
   )
